f7c9cf84d2aac8a00e24149fdb612e1cb23a15af
Cristian Zeni <cristian.zeni@marconirovereto.it>
2021-04-23 16:12:15 +0200

    aggiunto stile canvas

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
 
 context.beginPath();
-context.rect(10, 15, 80, 100);
-context.fillStyle = "FF0000";
+context.rect(240, 160, 20, 50);
+context.fillStyle = "Red";
 context.fill();
 context.closePath
