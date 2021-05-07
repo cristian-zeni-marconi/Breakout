5b74a2736b2f6bdf62df1a8f95599d6bb50d7389
Cristian Zeni <cristian.zeni@marconirovereto.it>
2021-05-07 15:34:39 +0200

    rimbalzo

--- js/game.js
+++ js/game.js
@@ -1,8 +1,32 @@
-let canvas = document.querySelector("canvas");
-let context = canvas.getContext("2d");
+var canvas = document.getElementById("breakout");
+var contex = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
-context.beginPath();
-context.rect(240, 160, 20, 50);
-context.fillStyle = "Red";
-context.fill();
-context.closePath
+function drawBall() {
+    contex.beginPath();
+    contex.arc(x, y, ballRadius, 0, Math.PI*2);
+    contex.fillStyle = "red";
+    contex.fill();
+    contex.closePath();
+}
+
+function draw() {
+    contex.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
