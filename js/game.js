17a6a458401779d29b7f9d263ead97fb73f67d36
Cristian Zeni <cristian.zeni@marconirovereto.it>
2021-05-07 16:05:42 +0200

    aggiunta racchetta

--- js/game.js
+++ js/game.js
@@ -5,11 +5,24 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
 
 function drawBall() {
     contex.beginPath();
     contex.arc(x, y, ballRadius, 0, Math.PI*2);
-    contex.fillStyle = "red";
+    contex.fillStyle = "black";
+    contex.fill();
+    contex.closePath();
+}
+
+function drawPaddle() {
+    contex.beginPath();
+    contex.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    contex.fillStyle = "black";
     contex.fill();
     contex.closePath();
 }
@@ -17,7 +30,8 @@ function drawBall() {
 function draw() {
     contex.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    
+    drawPaddle();
+
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
@@ -25,8 +39,42 @@ function draw() {
         dy = -dy;
     }
     
+    if(rightPressed) {
+        paddleX += 5;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftPressed) {
+        paddleX -= 5;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
+    
+
     x += dx;
     y += dy;
 }
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
 setInterval(draw, 10);
